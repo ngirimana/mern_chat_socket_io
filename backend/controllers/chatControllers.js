1e45135bd5cb98644cda4b4af0806f13f753651e
ngirimana <chadrackngirimana@gmail.com>
2022-05-01 10:37:30 +0200

    update group infos

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -120,4 +120,32 @@ const createGroupChat = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat, fetchChats, createGroupChat };
+/**
+ * @desc    Rename Group
+ *  @route   PUT /api/chat/rename
+ * @access  Protected
+ */
+
+const renameGroup = asyncHandler(async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName: chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+});
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { protect } = require('../middlewares/authMiddleware');
-const { accessChat, fetchChats,createGroupChat  } = require("../controllers/chatControllers");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+} = require("../controllers/chatControllers");
 
 const router = express.Router();
 router.route('/').post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
+router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
 // router.route("/groupadd").put(protect, addToGroup);
 
