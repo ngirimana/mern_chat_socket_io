f1dc41d3ae2caa3465773ecdf724092dec77b6df
ngirimana <chadrackngirimana@gmail.com>
2022-05-02 13:02:40 +0200

    display all messages

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -3,7 +3,23 @@ const Message = require("../models/messageModel");
 const User = require("../models/userModel");
 const Chat = require("../models/chatModel");
 
+/**
+ * @description     Get all Messages
+ * @route           GET /api/Message/:chatId
+ * @access          Protected
+ */
 
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name pic email")
+      .populate("chat");
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
 
 /**
  * @description     Create New Message
