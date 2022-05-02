e60634efa186578faaf9f889e5efd7379bc1109f
ngirimana <chadrackngirimana@gmail.com>
2022-05-02 13:18:42 +0200

    fix issue

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -42,10 +42,10 @@ const sendMessage = asyncHandler(async (req, res) => {
   };
 
   try {
-    const message = await Message.create(newMessage);
+    let message = await Message.create(newMessage);
 
-    message = await message.populate("sender", "name pic").execPopulate();
-    message = await message.populate("chat").execPopulate();
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
     message = await User.populate(message, {
       path: "chat.users",
       select: "name pic email",
