79f81cd7ec2093d7d0b85545ccd63dd645759008
ngirimana <chadrackngirimana@gmail.com>
2022-05-03 13:24:08 +0200

    fix isuue

--- frontend/src/components/authentication/Signup.js
+++ frontend/src/components/authentication/Signup.js
@@ -118,6 +118,7 @@ const Signup = () => {
       localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
       history.push("/chats");
+      window.location.reload();
     } catch (error) {
       toast({
         title: "Error Occured!",
