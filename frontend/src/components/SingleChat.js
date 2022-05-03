5d7ae52bf7930625d4b4c1f48fe4d36bab4f46b4
ngirimana <chadrackngirimana@gmail.com>
2022-05-03 12:41:35 +0200

    deployment steps

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://talk-a-live.herokuapp.com/"; 
 let socket, selectedChatCompare;
 
 const  SingleChat = ({ fetchAgain, setFetchAgain }) => {
